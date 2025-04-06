b321c714be7f43b98a35f436dacc70644337a094
sureshkumar <suresh@gmail.com>
2025-04-06 18:04:20 +0530

    updated on script

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,3 +3,6 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am DRAGON.... RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
+cowsay -f dog "Run for cover, I am DOG.... RAWR" >> dog.txt
+grep -i "dog" dog.txt
+cat dog.txt
